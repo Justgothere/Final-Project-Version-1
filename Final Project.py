e587b2542618285249efee9c47192cbcf30dd7d0
Justgothere <90232864+Justgothere@users.noreply.github.com>
2022-05-03 23:50:36 -0400

    Add files via upload

---
+++ Final Project.py
@@ -0,0 +1,16 @@
+# Program name: GarciaPeralta_David_FINALPROJECT
+# Author: David Garcia Peralta
+# Date: 3/1/2022
+# Summary: Add up the digits in a num
+# Variables:
+
+
+import random
+
+c_route = []
+
+
+for i in range(8):
+    val = random.randint(0, 4)
+    c_route = c_route.append(val) 
+    print("Correct route is: ", c_route)
